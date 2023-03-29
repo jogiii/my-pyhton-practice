fbdb49376cd775e8aa4977fde167f53bec4f56c9
jogi <joginder.kumar1991@gmail.com>
2023-03-29 22:16:29 +0530

    error specific except

--- day-30.py
+++ day-30.py
@@ -1,7 +1,13 @@
 # File no t found exception
 
 try:
+    #FileNotFoundError
     file = open("test.txt")
-except:
+    a_dictionary = {"key": "value"}
+    #KeyError
+    print(a_dictionary["test"])
+except FileNotFoundError:
     print("file created")
     file = open("test.txt", "w")
+except KeyError:
+    print("this key is not present in dictionary")

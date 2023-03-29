dcb1caaf6f05b2536ec64318927e63af13544544
jogi <joginder.kumar1991@gmail.com>
2023-03-29 22:23:02 +0530

    adding else part

--- day-30.py
+++ day-30.py
@@ -1,13 +1,16 @@
 # File no t found exception
 
 try:
-    #FileNotFoundError
+    # FileNotFoundError
     file = open("test.txt")
     a_dictionary = {"key": "value"}
-    #KeyError
-    print(a_dictionary["test"])
+    # KeyError
+    print(a_dictionary["key"])
 except FileNotFoundError:
     print("file created")
     file = open("test.txt", "w")
 except KeyError as error_message:
     print(f"this key {error_message} is not present in dictionary {a_dictionary}")
+else:
+    content = file.read()
+    print(content)
